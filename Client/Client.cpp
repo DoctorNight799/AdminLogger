524d84026de801b0974ab40b1e8b3a42957a7d6d
Night-Pc-Win <doctornight192@gmail.com>
2026-06-14 13:31:21 +0300

    User start

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,4 +1,39 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum Role {
+    Admin,
+    User
+};
+
+enum Status {
+    ACTIVE,
+    INACTIVE
+};
+
+class User {
+    string name;
+    Role role;
+    Status status;
+
+    User(string _name, Role _role, Status _status) : name(_name), role(_role), status(_status) {}
+
+    string toString();
+
+    void loadFromString(vector<string> data);
+};
+
+string User::toString() {
+
+}
+
+void User::loadFromString(vector<string> data) {
+
+}
+
 
 int main()
 {
